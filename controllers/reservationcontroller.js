ca41fdb1ae39e29882df212463fac479213285fe
ZenDLSU <160311924+ZenDLSU@users.noreply.github.com>
2026-03-13 01:21:45 +0800

    Add files via upload

--- controllers/reservationcontroller.js
+++ controllers/reservationcontroller.js
@@ -1,1 +1,24 @@
+// show available lab slots
+exports.viewSlots = (req, res) => {
+  res.render('reservation/viewslots');
+};
 
+// show reservations page
+exports.viewReservations = (req, res) => {
+  res.render('reservation/viewreservations');
+};
+
+// show reservation form
+exports.studentReserve = (req, res) => {
+  res.render('reservation/studentreserve');
+};
+
+// show technician reservation page
+exports.technicianReserve = (req, res) => {
+  res.render('reservation/technicianreserve');
+};
+
+// show edit reservation page
+exports.editReservation = (req, res) => {
+  res.render('reservation/editReservation');
+};
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,1 +1,28 @@
+// show login page
+exports.showLogin = (req, res) => {
+  res.render('user/login');
+};
 
+// show registration page
+exports.showRegistration = (req, res) => {
+  res.render('user/registration');
+};
+
+// show profile page
+exports.showProfile = (req, res) => {
+  res.render('user/profile');
+};
+
+// show edit profile page
+exports.showEditProfile = (req, res) => {
+  res.render('user/edit_profile');
+}
+
+// process login form
+exports.loginUser = (req, res) => {
+  const { username, password } = req.body;
+
+  console.log(username, password);
+
+  res.redirect('/user/profile');
+};
